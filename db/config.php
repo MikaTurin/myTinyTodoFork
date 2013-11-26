8c5c524ebe39ca5c71817e883c50882271665d47
MikaTurin <mika.turin@gmail.com>
2013-11-26 17:47:51 +0200

    first

---
+++ db/config.php
@@ -0,0 +1,25 @@
+<?php
+$config = array();
+$config['db'] = 'mysql';
+$config['mysql.host'] = 'DBHOST';
+$config['mysql.db'] = 'DBNAME';
+$config['mysql.user'] = 'DBUSER';
+$config['mysql.password'] = 'DBPWD&';
+$config['prefix'] = 'mtt_';
+$config['url'] = '';
+$config['mtt_url'] = '';
+$config['title'] = 'PLAN YTO';
+$config['lang'] = 'en';
+$config['password'] = '';
+$config['smartsyntax'] = 1;
+$config['timezone'] = 'Europe/Moscow';
+$config['autotag'] = 1;
+$config['duedateformat'] = 1;
+$config['firstdayofweek'] = 1;
+$config['session'] = 'files';
+$config['clock'] = 24;
+$config['dateformat'] = 'j M Y';
+$config['dateformat2'] = 'd.m.y';
+$config['dateformatshort'] = 'j M';
+$config['template'] = 'default';
+$config['showdate'] = 1;
---
+++ restrict.php
@@ -0,0 +1,10 @@
+<?php
+//die($_SERVER['REMOTE_ADDR']);
+
+/*$ips = array('192.168.0.1');
+
+if (!isset($_GET['loadLists']) && !isset($_GET['loadTasks']))
+{
+    if (!in_array($_SERVER['REMOTE_ADDR'], $ips)) die('IP not allowed!');
+}*/
+
